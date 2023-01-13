8875775c72ce0adaed3d2e22d6cd9e69621105e7
ustarry <121718468+ustarry@users.noreply.github.com>
2023-01-13 16:44:42 +0800

    Add files via upload

---
+++ js/articles.js
@@ -0,0 +1,453 @@
+const authors = 
+{
+    StarlightExplorer : 
+    {
+        name: "Starlight Explorer星光探索者",
+        intro: "探索星光，永无止境！",
+        avator: "/image/starlight_avator.jpg",
+    },
+    StarlightBureau : 
+    {
+        name: "Starlight Bureau星光情报局",
+        intro: "星光情报局，最快获得星光工作室的情报！",
+        avator: "/image/starlight_avator.jpg",
+    },
+    StarlightStudio : 
+    {
+        name: "Starlight Studio星光工作室",
+        intro: "✨仰望星空，漫天繁星不负缘✨💫追随光，奔赴梦，星光璀璨💫",
+        avator: "/image/starlight_avator.jpg",
+    },
+    Anran : 
+    {
+        name: "安仔仔哇",
+        intro: "期待与漫天繁星的相遇！",
+        avator: "https://image.mini1.cn/c/20210429/4b10a91ab913236bb7d82785c278eda1.png",
+    },
+    Qingyuan: 
+    {
+        name: "清 圆",
+        intro: "",
+        avator: "https://image.mini1.cn/d/20210810/6786a5fc52d0a1ce075d36c23aeddf26.png",
+    },
+    Kaikai: 
+    {
+        name: "凯凯本凯",
+        intro: "Always continue, Never breaks.",
+        avator: "https://image.mini1.cn/c/20200823/44452aa6e933a0b54f33f48243837a4c.png",
+    },
+    LuncheonMeat: 
+    {
+        name: "乖巧的午餐肉",
+        intro: "",
+        avator: "./image/only_tonight.png",
+    },
+    Mengchen: 
+    {
+        name: "Tatsuki丶梦辰",
+        intro: "",
+        avator: "https://image.mini1.cn/d/20210701/c7a3f5e9be7a496e6ba0a1eba29b0efa.jpg",
+    },
+    Yeshui: 
+    {
+        name: "夜水",
+        intro: "条条大路通罗马~",
+        avator: "https://qqchannel-profile-1251316161.file.myqcloud.com/1653310530f6ea13679f2463fa/100?t=1653310530",
+        // avator: "https://image.mini1.cn/d/20211113/2df6e176c93a08c7e0347535fcd0f339.jpg",
+    },
+    Lvxiaojian: 
+    {
+        name: "绿小箭",
+        intro: "",
+        avator: "https://qqchannel-profile-1251316161.file.myqcloud.com/16716269622f1db38513ece830/100?t=1671626962"
+    },
+    Baobaohendaimeng: 
+    {
+        name: "宝宝很呆萌",
+        intro: "",
+        avator: "https://rc-minichat.mini1.cn/rolehead/6.png"
+    },
+    TeacherMeow:
+    {
+        name: "喵喵",
+        intro: "已疯",
+        avator: "https://fb-cdn.fanbook.mobi/fanbook/app/files/service/headImage/13ad6d9c468b810f47cad1992bd5e5af.jpg"
+    },
+    Nanxi:
+    {
+        name: "南希",
+        intro: "嗨！这里是南希老头🌷 -主更生存建筑文案",
+        avator: "//p26.toutiaoimg.com/origin/user-avatar/77aab6985c885a3bd037df5feeb5de27"
+    },
+    Xiaohan:
+    {
+        name: "蓝孩纸小涵迷你",
+        intro: "",
+        avator: ""
+    },
+    HappyCrystal:
+    {
+        name: "HappyCrystal乐馨馨",
+        intro: "🌈人生苦短何必念念不忘 🙋Hi，欢迎来到我的世界 🍀笑一个吧，功成名就不是目的 🍒希望能给你带来快乐",
+        avator: "//sf1-cdn-tos.bdxiguastatic.com/img/user-avatar/ccee9646b81f25ff8faece241d48b5c3~240x240.image"
+    }
+}
+
+const articles = 
+[
+    {
+        author: authors.StarlightExplorer,
+        link: "/articles/p10.html",
+        title: "年轻人如何制作动态库静态库？",
+        date: "1/13/23",
+        intro: "年轻人想制作自己的静态库或动态库吗？来看看这篇文章吧！"
+    },
+    {
+        author: authors.HappyCrystal,
+        link: "https://v.ixigua.com/k5rqDqS/",
+        title: "山海终有归期，风雨终将重逢",
+        date: "1/12/23",
+        intro: "山海终有归期，风雨终将重逢，意难平终和解，万事终将如意"
+    },
+    {
+        author: authors.HappyCrystal,
+        link: "/articles/p9.html",
+        title: "春雨绵绵生存合集",
+        date: "1/12/23",
+        intro: "春雨绵绵生存，繁星闪烁，点亮星空"
+    },
+    {
+        author: authors.Nanxi,
+        link: "/articles/p8.html",
+        title: "一分钟建筑",
+        date: "1/12/23",
+        intro: "一分钟的短暂视频，建造多种风格的建筑"
+    },
+    {
+        author: authors.StarlightBureau,
+        link: "https://fanbook.mobi/circle/450153710870331392?ic=maANMF4E",
+        title: "以前的安安vs现在的安安",
+        date: "1/12/23",
+        intro: "以前的安安vs现在的安安"
+    },
+    {
+        author: authors.TeacherMeow,
+        link: "https://fanbook.mobi/circle/417151889004560384?ic=J3IYMFaD",
+        title: "UI编辑器：换装功能",
+        date: "1/12/23",
+        intro: "玩家点击装扮按钮换装成对应装扮"
+    },
+    {
+        author: authors.TeacherMeow,
+        link: "https://fanbook.mobi/circle/417151889004560384?ic=J3IYMFaD",
+        title: "UI编辑器：换装功能",
+        date: "1/12/23",
+        intro: "玩家点击装扮按钮换装成对应装扮"
+    },
+    {
+        author: authors.TeacherMeow,
+        link: "https://fanbook.mobi/circle/401917428260405248?ic=CQKjUFHl",
+        title: "触发器：玩家进入区域改变其外观",
+        date: "1/12/23",
+        intro: "使用触发器实现功能玩家进入区域改变其外观"
+    },
+    {
+        author: authors.TeacherMeow,
+        link: "https://fanbook.mobi/circle/417151889004560384?ic=J3IYMFaD",
+        title: "UI编辑器：换装功能",
+        date: "1/12/23",
+        intro: "玩家点击装扮按钮换装成对应装扮"
+    },
+    {
+        author: authors.Baobaohendaimeng,
+        link: "https://minichat.mini1.cn/share/sharePage.html?articleId=126652461197357056",
+        title: "触发器：晚上自动刷怪",
+        date: "1/12/23",
+        intro: "使用触发器制作晚上自动刷怪功能"
+    },
+    {
+        author: authors.Mengchen,
+        link: "https://minichat.mini1.cn/share/sharePage.html?articleId=159518510252617728",
+        title: "制作生存模式飞行触发器",
+        date: "1/12/23",
+        intro: "制作简易的生存飞行触发器"
+    },
+    {
+        author: authors.Mengchen,
+        link: "https://minichat.mini1.cn/share/sharePage.html?articleId=159518516317581312",
+        title: "触发器：每日签到系统",
+        date: "1/12/23",
+        intro: "使用触发器实现的每日签到系统玩法"
+    },
+    {
+        author: authors.Mengchen,
+        link: "https://minichat.mini1.cn/share/sharePage.html?articleId=159518511758372864",
+        title: "制作可以远程打开的懒人箱",
+        date: "1/12/23",
+        intro: "在玩生存的时候，经常会遇到制作某个非常复杂的东西，需要倒出跑翻箱子找材料，这样会浪费许多的时间，那么为什么不制作一个可以远程打开的箱子呢？"
+    },
+    {
+        author: authors.Mengchen,
+        link: "https://minichat.mini1.cn/share/sharePage.html?articleId=159518510252617728",
+        title: "制作生存模式飞行触发器",
+        date: "1/12/23",
+        intro: "制作简易的生存飞行触发器"
+    },
+    {
+        author: authors.Mengchen,
+        link: "https://dev-wiki.mini1.cn/m/creator/2221",
+        title: "UI编辑器：翻牌动画",
+        date: "1/12/23",
+        intro: "几个简单的步骤，轻松制作翻牌动画"
+    },
+    {
+        author: authors.Mengchen,
+        link: "https://dev-wiki.mini1.cn/m/creator/2045",
+        title: "UI编辑器：秒表",
+        date: "1/12/23",
+        intro: "秒表是日常生活中经常会使用到的一个计时工具，正常情况下，秒表一般只会计时到分钟单位，那么为何不使用UI编辑器在游戏中制作一个秒表呢？"
+    },
+    {
+        author: authors.Mengchen,
+        link: "https://dev-wiki.mini1.cn/m/creator/1642",
+        title: "UI编辑器：计算器",
+        date: "1/12/23",
+        intro: "计算器是平时经常使用的一个工具，那么要如何在迷你世界中制作一个计算器呢？为了解决这个问题，接下来我将会教学如何在迷你世界中制作一个属于自己的计算器"
+    },
+    {
+        author: authors.Qingyuan,
+        link: "https://dev-wiki.mini1.cn/m/creator/1707",
+        title: "UI编辑器：实时排行榜",
+        date: "1/12/23",
+        intro: "使用UI编辑器做的实时排行榜功能"
+    },
+    {
+        author: authors.Qingyuan,
+        link: "https://dev-wiki.mini1.cn/m/creator/1824",
+        title: "UI编辑器：拔河",
+        date: "1/12/23",
+        intro: "使用UI编辑器做的拔河玩法游戏"
+    },
+    {
+        author: authors.Qingyuan,
+        link: "https://dev-wiki.mini1.cn/m/creator/1639",
+        title: "UI编辑器：QTE快速反应",
+        date: "1/12/23",
+        intro: "使用UI编辑器做的快速反应玩法"
+    },
+    {
+        author: authors.Qingyuan,
+        link: "https://dev-wiki.mini1.cn/m/creator/1636",
+        title: "UI编辑器：密码门",
+        date: "1/12/23",
+        intro: "使用UI编辑器做的密码门"
+    },
+    {
+        author: authors.Qingyuan,
+        link: "/articles/p7.html",
+        title: "迷你世界lua脚本教学",
+        date: "1/12/23",
+        intro: "迷你世界lua脚本教学，帮助你快速使用脚本！"
+    },
+    {
+        author: authors.Qingyuan,
+        link: "/articles/p6.html",
+        title: "新手触发器讲解",
+        date: "1/12/23",
+        intro: "萌新应如何理解触发器呢？让我们一起看看吧！"
+    },
+    {
+        author: authors.Lvxiaojian,
+        link: "https://pd.qq.com/s/1md8i3ah4?shareSource=5",
+        title: "视频创作攻略-游戏领域创作的一些事",
+        date: "1/12/23",
+        intro: "本文介绍了作为一个游戏领域创作者，在视频创作方面需要注意的地方"
+    },
+    {
+        author: authors.Yeshui,
+        link: "https://pd.qq.com/s/7u6zu69n5?shareSource=5",
+        title: "迷你世界地图制作导航之地图框架",
+        date: "1/12/23",
+        intro: "制作地图没有头绪？本导航将指引你制作地图！"
+    },
+    {
+        author: authors.StarlightBureau,
+        link: "/articles/p5.html",
+        title: "安安实际上是隐藏大佬？",
+        date: "1/12/23",
+        intro: "看安安小姐姐教你如何运用触发器和脚本制作丰富玩法！"
+    },
+    {
+        author: authors.Anran,
+        link: "https://fanbook.mobi/circle/396094214493974528",
+        title: "成为官方社区管理这一个多月里，我学会了什么？",
+        date: "1/12/23",
+        intro: "有很多东西，是刷题，学习，看书，旅游所学不到的，在社区的这段日子，哪怕只有6.7.8这两个多月的时间，我学到了很多哎"
+    },
+    {
+        author: authors.Anran,
+        link: "https://v.ixigua.com/k56vtrA/",
+        title: "就算这次错过也只是怕错过",
+        date: "1/12/23",
+        intro: "有点时候，失去了才懂得珍惜"
+    },
+    {
+        author: authors.Anran,
+        link: "https://v.ixigua.com/k56cXhR/",
+        title: "这一刻，我不再迷茫！",
+        date: "1/12/23",
+        intro: "人生路漫漫。因为星光，但这一刻我感受到做视频的快乐"
+    },  
+    {
+        author: authors.Anran,
+        link: "https://fanbook.mobi/circle/404604923607515136?ic=maANMF4E",
+        title: "将热爱变成一门职业",
+        date: "1/12/23",
+        intro: "因为热爱，所以发光。因为社区和星光，我喜欢上了文创。"
+    },    
+    {
+        author: authors.Anran,
+        link: "https://v.ixigua.com/k56Tmq2/",
+        title: "离大谱！",
+        date: "1/12/23",
+        intro: "我是万万没有想到啊！水帖还能水成......"
+    },  
+    {
+        author: authors.Anran,
+        link: "https://pd.qq.com/s/97rl8bn96?shareSource=5",
+        title: "古风建筑入门？看这个！",
+        date: "1/12/23",
+        intro: "如何制作古风建筑尼？来看看这个文章叭！"
+    },    
+    {
+        author: authors.Anran,
+        link: "https://v.ixigua.com/k5MbhwR/",
+        title: "当你觉得周围的人不够努力时，来看看这个视频吧！",
+        date: "1/12/23",
+        intro: "当你觉得周围的人不够努力时，看看这个视频。"
+    },   
+    {
+        author: authors.Anran,
+        link: "https://fanbook.mobi/circle/448088685787742208",
+        title: "再见2022,你好2023！",
+        date: "1/12/23",
+        intro: "回顾2022的这一年，也许，我的收获与成长，是一只脚迈入了可靠的大门叭"
+    },    
+    {
+        author: authors.Anran,
+        link: "/articles/p5.html",
+        title: "安安滴建筑小课堂持续进行中！",
+        date: "1/12/23",
+        intro: "安安滴建筑小课堂持续开课！花几分钟的时间入门古风建筑！"
+    },   
+    {
+        author: authors.Anran,
+        link: "/articles/p4.html",
+        title: "安安滴建筑小课堂开课咯",
+        date: "1/12/23",
+        intro: "安安滴建筑小课堂持续开课！几分钟的视频，学会建造好看房子鸭"
+    },    
+    {
+        author: authors.StarlightBureau,
+        link: "/articles/p3.html",
+        title: "中考备考感到困难？安安教你如何备考！",
+        date: "1/12/23",
+        intro: "中考是人生关键的转折点，安安小姐姐教你如何备战中考！"
+    },
+    {
+        author: authors.StarlightBureau,
+        link: "/articles/p2.html",
+        title: "劲爆！安安小姐姐的日记不慎流出？",
+        date: "1/12/23",
+        intro: "作为星光工作室的副团长，安安对星光的孩纸操碎了心。点击查看安安小姐姐的奋斗历程！"
+    },
+    {
+        author: authors.Kaikai,
+        link: "https://dev-wiki.mini1.cn/creator/2037",
+        title: "UI编辑器：通兑卷系统",
+        date: "1/12/23",
+        intro: "为应对2022年1月19日起施行的商业化设置调整，制作本教程。"
+    },
+    {
+        author: authors.Kaikai,
+        link: "https://dev-wiki.mini1.cn/creator/2042",
+        title: "UI编辑器：自定义小地图系统",
+        date: "1/12/23",
+        intro: "自定义小地图，丰富你的游戏！"
+    },
+    {
+        author: authors.Kaikai,
+        link: "https://pd.qq.com/s/5a5bwgbff?shareSource=5",
+        title: "玩法教学：自动保存存档点",
+        date: "1/12/23",
+        intro: "在跑酷地图中我们经常需要保存存档点，本案例可以使你的跑酷地图自动保存存档点。"
+    },
+    {
+        author: authors.Kaikai,
+        link: "https://dev-wiki.mini1.cn/creator/2009",
+        title: "UI编辑器：兑换码系统",
+        date: "1/12/23",
+        intro: "学会这个，为你的地图增添兑换码功能！"
+    },
+    {
+        author: authors.Kaikai,
+        link: "https://pd.qq.com/s/beu2to31q?shareSource=5",
+        title: "UI编辑器：登录系统增强",
+        date: "1/12/23",
+        intro: "简单几个步骤，设计出一个登录系统。"
+    },
+    {
+        author: authors.Kaikai,
+        link: "https://dev-wiki.mini1.cn/creator/1992",
+        title: "UI编辑器：登录系统",
+        date: "1/12/23",
+        intro: "简单几个步骤，设计出一个登录系统。"
+    },
+    {
+        author: authors.StarlightBureau,
+        link: "/articles/p1.html",
+        title: "星光一周年精彩文章",
+        date: "1/12/23",
+        intro: "星光一周年精彩文章。现在已是星光二周年，但星光一周年的欢乐时光仿佛还在昨天！"
+    },
+    {
+        author: authors.Anran,
+        link: "/articles/p0.html",
+        title: "我们不一定要追风鸭!",
+        date: '23/1/11',
+        intro: "要知道，跑步前进最多只能跑几十公里，而选择埋头走路，却可以走出两万五千里长征,无论游戏，还是学习都是如此鸭",
+    },
+    {
+        author: authors.StarlightStudio,
+        link: "/about.html",
+        title: "Invitaion from the Starlight Studio",
+        date: '23/1/11',
+        intro: "Hello guys! the Starlight Studio wants you to join!"
+    },
+]
+
+
+
+function LoadArticleInfo(articles, content)
+{
+    var title = document.createElement("h1");
+    var avator = document.createElement("img");
+    var info = document.createElement("div");
+    var intro = document.createElement("p");
+    var name = document.createElement("p");
+    var views = document.createElement("p");
+    info.appendChild(name);
+    info.appendChild(intro);
+    title.innerText = content.title;
+    avator.src = content.author.avator;
+    intro.className = "intro text_one_line";
+    name.className = "name text_one_line";
+    intro.innerText = content.author.intro;
+    name.innerText = content.author.name;
+    views.innerHTML = "Date: " + content.date + " Views: <span id=\"busuanzi_value_page_pv\">loading...</span>"
+    articles.appendChild(title);
+    articles.appendChild(views);
+    articles.appendChild(avator);
+    articles.appendChild(info);
+    document.getElementsByTagName("title")[0].innerText = content.title + " | Starlight Zone"
+}
---
+++ js/main.js
@@ -0,0 +1,45 @@
+var menu_showed = false;
+
+function show_menu()
+{
+    var menu_list = document.getElementById("menu_list");
+    if (menu_showed)
+    {
+        menu_list.className = "hidden_menu";
+    }
+    else
+    {
+        menu_list.className = "showed_menu";
+    }
+    menu_showed = !menu_showed;
+}
+
+
+function on_search(self)
+{
+    alert("搜索无结果!")
+}
+
+function GetCurLanguage()
+{
+    if (localStorage.getItem("lang") == undefined)
+    {
+        localStorage.setItem("lang", navigator.language.substring(0, 2))
+    }
+    return localStorage.getItem("lang");
+}
+
+
+
+window.addEventListener("load", function()
+{
+    if (window.navigator.userAgent.indexOf("MSIE")>=1)
+    {
+        alert("Your Internet Explorer's version is less than IE 11, so some pages might not be loaded well.")
+    }
+    var result = new URLSearchParams(new URL(this.window.location).search);
+    if (result.get("lang"))
+    {
+        this.localStorage.setItem("lang", result.getItem("lang"))
+    }
+})
